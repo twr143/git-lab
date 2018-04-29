2c94d73b7384e92c06b453e313d7c6575ca86e78
twr143 <twr143@gmail.com>
2018-04-29 18:02:47 +0300

    m clean up

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,10 +3,5 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        //m 1
-        //m 2
-        // b3 1
-        // b3 2
     }
 }
